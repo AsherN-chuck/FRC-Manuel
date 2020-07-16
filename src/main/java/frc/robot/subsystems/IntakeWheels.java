729d3847bd06e55d6f58dde9292fc59d4c7daa73
AsherN-chuck <NieffAsh000@students.wvsd.org>
2020-07-16 16:30:51 -0700

    Added some Intake code

--- src/main/java/frc/robot/subsystems/IntakeWheels.java
+++ src/main/java/frc/robot/subsystems/IntakeWheels.java
@@ -1,5 +1,17 @@
-package frc.robot.subsystems; 
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class IntakeWheels {
-    
+    private final SpeedController moter = new Spark(3);
+}
+
+public void Grab() {
+   moter.set(0.5);
+}
+public void Release(){
+    moter.set(-0.5);
+}
+public boolean Holdstop(){
 }
