1a9ba2284eefc43646a23834904dea8467647987
AsherN-chuck <NieffAsh000@students.wvsd.org>
2020-07-20 18:40:29 -0700

    Added drivecommandjoystick code

--- src/main/java/frc/robot/commands/DriveCommandJoystick.java
+++ src/main/java/frc/robot/commands/DriveCommandJoystick.java
@@ -1,9 +1,27 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
 
 
-public class DriveCommandJoystick {
-    private Joystick joystick = new Joystick(0);
-    private Joystick joystick2 = new Joystick(1);
+public class DriveCommandJoystick extends CommandBase {
+    private DrivetrainSubsystem drivetrain;
+    private DoubleSupplier rotationSpeed; 
+    private DoubleSupplier forwardSpeed;
+
+    public DriveCommandJoystick(DrivetrainSubsystem drivetrain, DoubleSupplier rotationSpeed, DoubleSupplier forwardSpeed) {
+        this.rotationSpeed = rotationSpeed;
+        this.forwardSpeed = forwardSpeed;
+        this.drivetrain = drivetrain;
+
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void execute(){
+        drivetrain.arcade(forwardSpeed.getAsDouble(), rotationSpeed.getAsDouble());
+    }
 }
