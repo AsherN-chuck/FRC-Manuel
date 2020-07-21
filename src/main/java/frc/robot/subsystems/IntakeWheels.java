d1a55c6fe91bd7385be17269fc2ad5be3850dc47
AsherN-chuck <NieffAsh000@students.wvsd.org>
2020-07-20 18:41:23 -0700

    added intakewheels code

--- src/main/java/frc/robot/subsystems/IntakeWheels.java
+++ src/main/java/frc/robot/subsystems/IntakeWheels.java
@@ -1,16 +1,28 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeWheels {
-    private final SpeedController moter = new Spark(3);
-}
-public void Grab() {
-   moter.set(0.5);
-}
-public void Release(){
-    moter.set(-0.5);
-}
-public boolean Holdstop(){
+public class IntakeWheels extends SubsystemBase{
+    private final SpeedController intakeMotor = new VictorSP(3);
+    private AnalogInput ballSensor = new AnalogInput(0);
+    public void Grab() {
+        intakeMotor.set(0.5);
+    }
+    public void Release(){
+        intakeMotor.set(-0.5);
+    }
+    public void stop() {
+        if (hasBall()) {
+            intakeMotor.set(0.1);
+        } else {
+            intakeMotor.set(0);
+        }
+    }
+    
+    public boolean hasBall() {
+        return ballSensor.getVoltage() < 2.0;
+    }
 }
