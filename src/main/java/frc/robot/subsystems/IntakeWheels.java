493d374fa14e34f83e2b9a9221cfe16d7c7c045d
AsherN-chuck <NieffAsh000@students.wvsd.org>
2020-07-22 01:28:50 -0700

    Removed some code that wasn't part of the robot.

--- src/main/java/frc/robot/subsystems/IntakeWheels.java
+++ src/main/java/frc/robot/subsystems/IntakeWheels.java
@@ -1,13 +1,11 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeWheels extends SubsystemBase{
     private final SpeedController intakeMotor = new VictorSP(3);
-    private AnalogInput ballSensor = new AnalogInput(0);
     public void Grab() {
         intakeMotor.set(0.5);
     }
@@ -15,14 +13,6 @@ public class IntakeWheels extends SubsystemBase{
         intakeMotor.set(-0.5);
     }
     public void stop() {
-        if (hasBall()) {
-            intakeMotor.set(0.1);
-        } else {
-            intakeMotor.set(0);
-        }
-    }
-    
-    public boolean hasBall() {
-        return ballSensor.getVoltage() < 2.0;
+        intakeMotor.set(0);
     }
 }
