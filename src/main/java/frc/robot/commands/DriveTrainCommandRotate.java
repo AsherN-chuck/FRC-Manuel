1d4c18e6984f612a6dbf0958733888b997f4db2d
AsherN-chuck <NieffAsh000@students.wvsd.org>
2020-08-19 19:56:02 -0700

    Added rotation command for the DriveTrain

---
+++ src/main/java/frc/robot/commands/DriveTrainCommandRotate.java
@@ -0,0 +1,52 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class DriveTrainCommandRotate extends CommandBase {
+    private DrivetrainSubsystem drivetrain;
+    private double targetPosition;
+    private PIDController pidController = new PIDController(0.0001, 0, 0);
+    private TrapezoidProfile profile;
+    private double startTime;
+    private double power; 
+    private Gyro gyroscope;
+
+    public DriveTrainCommandRotate(DrivetrainSubsystem drivetrain, double targetPosition) {
+        addRequirements(drivetrain);
+        this.drivetrain = drivetrain;
+        this.targetPosition = targetPosition;
+    }
+
+    @Override
+    public void initialize() {
+        profile = new TrapezoidProfile(
+            new TrapezoidProfile.Constraints(5, 10),
+            new TrapezoidProfile.State(targetPosition, 0),
+            new TrapezoidProfile.State(gyroscope.getAngle(), drivetrain.getVelocity())
+        );
+        startTime = Timer.getFPGATimestamp();
+    }
+
+    @Override
+    public boolean isFinished() {
+        double currentTime = Timer.getFPGATimestamp() - startTime;
+        return Math.abs(gyroscope.getAngle() - targetPosition) < 0.5 && currentTime > profile.totalTime();
+    }
+
+    @Override
+    public void execute() {
+        power = pidController.calculate(gyroscope.getAngle(), targetPosition);
+        TrapezoidProfile.State scheduledState = profile.calculate(Timer.getFPGATimestamp() - startTime);
+
+        power =
+            0.5 * scheduledState.velocity +
+            pidController.calculate(gyroscope.getAngle(), scheduledState.position);
+
+        drivetrain.setSpeed(power);
+    }
+}
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.sensors.CANCoder;
+
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -8,10 +10,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DrivetrainSubsystem extends SubsystemBase {
     private SpeedController leftMoter = new Spark(0);
     private SpeedController rightMoter = new Spark(1);
-
+    private double speed;
     private DifferentialDrive DrivetrainSubsystem = new DifferentialDrive(leftMoter, rightMoter);
+    private CANCoder encoder = new CANCoder(2); 
 
     public void arcade(double forwardSpeed, double rotationSpeed){
         DrivetrainSubsystem.arcadeDrive(forwardSpeed, rotationSpeed);
+    }
+
+	public void setSpeed(double speed){
+        this.speed = speed;
+        
+    }
+
+	public double getVelocity(){
+        return encoder.getVelocity();
+    }
+
+    @Override
+    public void periodic(){
+        leftMoter.set(speed);
+        rightMoter.set(speed);
+        speed = 0;
     }
 }
